4f2ce2cefa8c8146c71eea3abb4e1b2724d84e1d
ddongseop <dlehdtjq00o@gmail.com>
2023-06-08 23:21:36 +0900

    [Section1] 스프링 데이터 JPA와 DB 설정, 동작확인

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+//@Setter // 가급적 @Setter 보다는 의미있는 네이밍의 method 사용
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public void changeUsername(String username) {
+        this.username = username;
+    }
+}
