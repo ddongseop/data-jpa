2af124a3d39a32f34c164f757e96cdcd8d1325c5
ddongseop <dlehdtjq00o@gmail.com>
2023-06-26 16:39:27 +0900

    [Section5] Web 확장

--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data //Entity에는 @Gettter @Setter 다 들어가 있는 @Data 안쓰는게 좋음
 public class MemberDto {
@@ -13,5 +14,10 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
